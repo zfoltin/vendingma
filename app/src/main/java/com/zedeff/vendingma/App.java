dc4564ffc55f08dbfc8b80921637d6c73df404b3
Zeno Foltin <zeno.foltin@gmail.com>
2015-10-19 08:58:10 +0100

    Remove test data

--- app/src/main/java/com/zedeff/vendingma/App.java
+++ app/src/main/java/com/zedeff/vendingma/App.java
@@ -2,25 +2,12 @@ package com.zedeff.vendingma;
 
 import android.app.Application;
 
-import com.zedeff.vendingma.models.Item;
 import com.zedeff.vendingma.services.VendingMachine;
 import com.zedeff.vendingma.services.VendingMachineImpl;
-
-import java.math.BigDecimal;
 
 public class App extends Application {
 
     private static VendingMachine vendingMachine = new VendingMachineImpl();
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        // TODO: remove this - testing only
-        vendingMachine.addStock(new Item("Cool Coke", new BigDecimal("1.75")), 2);
-        vendingMachine.addStock(new Item("Bitter beer", new BigDecimal("3.75")), 1);
-        vendingMachine.addStock(new Item("Empty bucket", new BigDecimal("0.15")), 0);
-    }
 
     public static VendingMachine getVendingMachine() {
         return vendingMachine;
